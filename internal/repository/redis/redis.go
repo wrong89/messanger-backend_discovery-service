1e1b461aecf6137e3e06312bb2387418267c96ba
wrong89 <fenomen_03@mail.ru>
2026-02-03 02:40:25 +0300

    implement repo in redis.go

--- internal/repository/redis/redis.go
+++ internal/repository/redis/redis.go
@@ -1,6 +1,12 @@
 package redis
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"strings"
+	"wrong89/messanger-backend_discovery-service/internal/models"
+
+	"github.com/redis/go-redis/v9"
+)
 
 type Storage struct {
 	db *redis.Client
@@ -15,4 +21,28 @@ func NewStorage(dbURL string) Storage {
 	return Storage{
 		db: redis.NewClient(opts),
 	}
+}
+
+func (s *Storage) Close() error {
+	return s.db.Close()
+}
+
+func (s *Storage) WriteServer(ctx context.Context, server models.Server) error {
+	return s.db.Set(ctx, server.Title, server.GetAddress(), 0).Err()
+}
+
+func (s *Storage) GetServerByTitle(ctx context.Context, title string) (models.Server, error) {
+	var result models.Server
+
+	addr, err := s.db.Get(ctx, title).Result()
+	if err != nil {
+		return models.Server{}, err
+	}
+
+	args := strings.Split(addr, ":")
+	result.Title = title
+	result.Host = args[0]
+	result.Port = args[1]
+
+	return result, nil
 }
